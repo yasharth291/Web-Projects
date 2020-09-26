c33551ba04912399ffd0936c14bdf963dc644067
Yasharth dubey <yashdubeywinner@gmail.com>
2020-09-26 23:55:56 +0530

    Add files via upload

---
+++ Quiz/js/userinfo.js
@@ -0,0 +1,6 @@
+let name = sessionStorage.getItem("name");
+let points  = sessionStorage.getItem("points");
+let time = sessionStorage.getItem("time");
+
+document.querySelector(".name").innerHTML = name;
+document.querySelector(".points").innerHTML = points;
